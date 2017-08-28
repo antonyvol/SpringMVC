2d2e929e926d0a52640b3372a5bafc43ba9f3e24
antonyvol <antonyvol1@gmail.com>
2017-08-28 16:21:14 +0300

    Added test index.html page. Works on Tomcat v.8.5.20

--- SpringApp/main/java/org/antonyvol/mvc/HelloWorldController.java
+++ SpringApp/main/java/org/antonyvol/mvc/HelloWorldController.java
@@ -3,15 +3,18 @@ package org.antonyvol.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.portlet.mvc.ParameterizableViewController;
 
 /**
  * Created by Антон on 27.08.2017.
  */
 
 @Controller
-public class HelloWorldController {
+@RequestMapping("/helloworld")
+public class HelloWorldController extends ParameterizableViewController {
     // mapping on the according "path" on the mvc-config.xml
-    @RequestMapping("/helloworld")
+    @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("msg", "this is hello from the controller!");
         // return the path to the according jsp
